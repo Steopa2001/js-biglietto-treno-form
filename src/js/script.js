4b1a54364b2f8bdae298afc5437959c2feb83a2f
Steopa2001 <moraru495@gmail.com>
2025-07-10 17:16:38 +0200

    feat: finish MILESTONE 2

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+document.getElementById('calcola').addEventListener('click', function() {
+        const nome = document.getElementById('nome').value;
+        const km = document.getElementById('km').value;
+        const age = document.getElementById('age').value;
+        const output = document.getElementById('output');
+
+        // Validazione base: km e age devono essere numeri > 0
+        if (km > 0 && age > 0) {
+          let prezzo = km * 0.21;
+
+          if (age < 18) {
+            prezzo *= 0.8;
+          } else if (age > 65) {
+            prezzo *= 0.6;
+          }
+
+          // Formattazione a due decimali
+          prezzo = prezzo.toFixed(2);
+
+          // Mostra in pagina
+          output.innerHTML = `
+            <p>NomeCognome: ${nome}</p>
+            <p>Kilometri: ${km}</p>
+            <p>Età: ${age}</p>
+            <p>Prezzo del biglietto: € ${prezzo}</p>
+          `;
+        } else {
+          output.innerHTML = '<p style="color:red;">Per favore inserisci valori validi maggiori di 0.</p>';
+        }
+      });

ee851adebf9040eadd5fb79ecf8112732f539d07
Steopa2001 <moraru495@gmail.com>
2025-07-11 15:57:47 +0200

    fix: correction

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,60 @@
-document.getElementById('calcola').addEventListener('click', function(e) {
-    e.preventDefault();
-    
-        const nome = document.getElementById('nome').value;
-        const km = document.getElementById('km').value;
-        const age = document.getElementById('age').value;
-        const output = document.getElementById('output');
+// recupero il pulsante dal DOM
+const calcola = document.getElementById("calcola");
+const annulla = document.getElementById("annulla");
 
+//recupero il div in cui mostrare il risultato
+const risultato = document.getElementById("result");
 
-        // Validazione base: km e age devono essere numeri > 0
-        if (km > 0 && age > 0) {
-          let prezzo = km * 0.21;
+//facciamo rimanere in ascolto dell'evento click il pulsante
+calcola.addEventListener("click", (e) => {
+  e.preventDefault();
 
-          if (age < 18) {
-            prezzo *= 0.8;
-          } else if (age > 65) {
-            prezzo *= 0.6;
-          }
+  // vado a recuperare i valori dei campi della form
+  const km = document.getElementById("km").value;
+  const eta = document.getElementById("eta").value;
+  const name = document.getElementById("name").value;
 
-          // Formattazione a due decimali
-          prezzo = prezzo.toFixed(2);
+  // logica
+  // calcoliamo il prezzo base
+  const basePrice = 0.21 * km;
 
-          // Mostra in pagina
-          output.innerHTML = `
-            <p>Nome e Cognome: ${nome}</p>
-            <p>Chilometri: ${km}</p>
-            <p>Età: ${age}</p>
-            <p>Prezzo del biglietto: € ${prezzo}</p>
-          `;
-        } else {
-          output.innerHTML = '<p style="color:red;">Per favore inserisci valori validi maggiori di 0.</p>';
-        }
-      });
+  let prezzoFinale = 0;
+
+  if (eta < 18) {
+    prezzoFinale = basePrice * 0.8;
+  } else if (eta < 65) {
+    prezzoFinale = basePrice * 0.6;
+  } else {
+    prezzoFinale = basePrice;
+  }
+
+  risultato.innerHTML = `
+  <table border="1">
+      <thead>
+        <tr>
+          <th>Nome e Cognome</th>
+          <th>Km da percorrere</th>
+          <th>Età del passeggero</th>
+          <th>Prezzo finale (€)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>${name}</td>
+          <td>${km} km</td>
+          <td>${eta}</td>
+          <td>${prezzoFinale.toFixed(2)} €</td>
+        </tr>
+      </tbody>
+    </table>`;
+});
+
+annulla.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //reset del form
+    document.getElementById('myForm').reset();
+
+    //pulizia del risultato 
+     risultato.innerHTML = '';
+});
